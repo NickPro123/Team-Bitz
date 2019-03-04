c4b4f2710bff07df27534b93a743977045dc595e
DanielleAlloy <44452123+DanielleAlloy@users.noreply.github.com>
2019-03-04 14:30:53 -0500

    Add files via upload

--- Sprint1 (Version 1.4)/primecare/main.php
+++ Sprint1 (Version 1.4)/primecare/main.php
@@ -1,65 +1,81 @@
-<?php 
-// I certify that this submission is my own original work
-//Joseph Mineo
-//R01581419
+<?php
+
 session_start();
 require_once 'functions.php';
-
-
-if (!empty($_SESSION['user']))
-  {
-
-echo "
-    <link rel='stylesheet' type='text/css' href='css/style.css'>
-    
-  <header>
-      <img src='images/logo.png' class='logo'>
-  </header>
-    <body>
-      <th colspan='2' align='center'>Main Menu</th>";
-      
-      $result = queryMysql("select * from patient");
-  if ($result->num_rows > 0)
-  {
-   echo "<style>
-        table {
-            border-collapse: collapse;
-        }
-                   
-        </style>
-      
-      <table border = '1'>
-  <tr>
-      <th>ID</th>
-    <th>Name</th>
-    <th>Room Number</th> 
-    
-  </tr>";
-    while ($row = mysqli_fetch_assoc($result)){
-  echo "<tr>
-    <th>".$row["patientID"]."</th> 
-    <th>".$row["firstName"]." ".$row["lastName"]."</th>
-    <th>".$row["roomNumber"]."</th></tr>";
-            
-    }
-echo "    
-      
-    </table>
-      </form>
-            <form method='post' action='logout.php' onsubmit='return true'>
-        <tr><td colspan='2' align='center'><input type='submit' value='Logout'></td></tr>
-
-      
-    </table>
-  </body>";
-  }else{
-     echo "There is no data to be displayed please <a href='main.php'>add</a> some.";
-  }
-  
-  }else{
-  echo "You are not logged in
-  <a href='Signup.html'>click here</a> to refresh the screen.";
-  }
+if(!isset($_SESSION['user'])){
+    header("Location:login.php");
+}
+$result = queryMysql("select * from patient")
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato|Raleway:400,700,900" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Prime Health Care - Welcome</title>
+    </head>
+
+    <!--Navbar-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.html">Prime Care</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+            <!--Logout Button-->
+            <div class="form-inline my-2 my-lg-0">
+            <form method='post' action='logout.php' onsubmit='return true'>
+                <input type='submit' value='Logout'>
+            </div>
+        </div>
+    </nav>
+    <header>
+        <img src="images/logo.png" class="logo">
+    </header>
+    <div class="container-fluid">
+        <div class="mx-auto">
 
 
+
+          <h1>Main Menu</h1>
+
+         <!-- <//?php $result = queryMysql("select * from patient");?>-->
+      <?php if ($result->num_rows > 0)
+      {
+          ?>
+
+          <table border = '1'>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Room Number</th>
+      </tr>
+        <?php
+        while ($row = mysqli_fetch_assoc($result)){
+            ?>
+        <tr>
+            <td><?php echo "$row[patientID]";?></td>
+            <td><?php echo "$row[firstName] $row[lastName]"; ?></td>
+            <td><?php echo "$row[roomNumber]";?></td>
+        </tr>
+            <?php ; } ?>
+      </table>
+        </div>
+      <!--}else{
+         echo "There is no data to be displayed please <a href='main.php'>add</a> some.";
+      }-->
+<?php } ?>
+
+        <!-- Optional JavaScript -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </div>
+</html>
