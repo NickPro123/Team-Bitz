882f45eb697085d10dbdbdd4e528c932fd6c50da
NickPro123 <48159789+NickPro123@users.noreply.github.com>
2019-03-11 11:18:15 -0400

    fixed names of tables
    Fixed the names of the tables to be lowercase.

--- Sprint1 (Version 1.4)/primecare/MYSQL(PROD) database backup files/New_Tables_Querry.sql
+++ Sprint1 (Version 1.4)/primecare/MYSQL(PROD) database backup files/New_Tables_Querry.sql
@@ -1,5 +1,5 @@
 -- Creates treatment table
-CREATE table TREATMENT(
+CREATE table treatment(
 treatmentID	int	primary key	not null auto_increment,
 treatmentName	varchar(100)	not null,
 recommendedAmount	decimal(20,1)	not null
@@ -7,47 +7,47 @@ recommendedAmount	decimal(20,1)	not null
 ALTER TABLE TREATMENT auto_increment = 1;
 
 -- Creates PatientAssignedToTreatment table
-create table PatientAssignedToTreatment(
+create table patientassignedtotreatment(
 patientID	int not null,
 treatmentID	int not null,
 primary key(patientID,treatmentID),
-constraint FK_patientID foreign key(patientID) references PATIENT(patientID) on update no action on delete no action,
-constraint FK_treatmentID	foreign key(treatmentID) references TREATMENT(treatmentID) on update no action on delete no action
+constraint FK_patientID foreign key(patientID) references patient(patientID) on update no action on delete no action,
+constraint FK_treatmentID	foreign key(treatmentID) references treatment(treatmentID) on update no action on delete no action
 );
 
 -- Creates Test table
-create table TEST(
+create table test(
 testID	int	primary key	not null auto_increment,
 testName	varchar(100)	not null,
 testResult	varchar(100)	null
 );
-ALTER TABLE TEST auto_increment = 1;
+ALTER TABLE test auto_increment = 1;
 
 -- Creates PatientAssignedToTest table
-create table PatientAssignedToTest(
+create table patientassignedtotest(
 patientID	int not null,
 testID	int	not null,
 primary key(patientID,testID),
-constraint FK_patientID foreign key(patientID) references PATIENT(patientID) on update no action on delete no action,
-constraint FK_testID foreign key(testID) references TEST(testID) on update no action on delete no action
+constraint FK_patientID foreign key(patientID) references patient(patientID) on update no action on delete no action,
+constraint FK_testID foreign key(testID) references test(testID) on update no action on delete no action
 );
 
 -- Fills treatment table with data
-insert into TREATMENT(treatmentName,recommendedAmount) VALUES
+insert into treatment(treatmentName,recommendedAmount) VALUES
 ('treatment1', 1.5),
 ('treatment2', 2.5);
 
 -- Fills PatientAssignedToTreatment table with data
-insert into PatientAssignedToTreatment(patientID,treatmentID) VALUES
+insert into patientassignedtotreatment(patientID,treatmentID) VALUES
 (1,1),
 (2,2);
 
 -- Fills Test table with data
-insert into TEST(testName,testResult) VALUES
+insert into test(testName,testResult) VALUES
 ('Test1','Healthy'),
 ('Test2','Cancer');
 
 -- Fills PatientAssignedToTest table with data
-insert into PatientAssignedToTest(patientID,testID) VALUES
+insert into patientassignedtotest(patientID,testID) VALUES
 (1,1),
 (2,2);
